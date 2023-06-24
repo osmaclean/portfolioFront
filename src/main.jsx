c8ff91efd55d38608a189850c556d7e9b4f39901
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-06-24 16:48:04 -0300

    infra: first setup

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import "./index.scss";
+import "./styles/index.css";
+import Home from "../src/views/Home/Home";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
