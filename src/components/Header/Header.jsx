c9ee314a049ff7f76b353da49ff7155ca25a47d4
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-07-02 12:49:40 -0300

    fix: adjust color text

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,11 @@ export default function Header() {
   return (
     <header className="h-14 w-full bg-header shadow-md p-6 flex items-center justify-center fixed top-0 z-50">
       <nav className="flex items-center justify-between w-11/12">
-        <h1 className="text-2xl font-bold cursor-pointer">LUCAS MACLEAN</h1>
-        <ul className="flex items-center justify-center gap-6 text-sm">
-          <li className="cursor-pointer hover:opacity-50 ease-in-out duration-500 font-normal">
+        <h1 className="text-2xl font-bold cursor-pointer text-content">
+          LUCAS MACLEAN
+        </h1>
+        <ul className="flex items-center justify-center gap-6 text-sm text-content">
+          <li className="cursor-pointer hover:opacity-50 ease-in-out duration-500 font-normal ">
             Portfólio
           </li>
           <li className="cursor-pointer hover:opacity-50 ease-in-out duration-500 font-normal">
