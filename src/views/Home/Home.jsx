48588fda34435ded2dcbc6f94479b7255df81819
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-07-03 18:32:42 -0300

    feat: added structure and style of Work component

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,12 +2,12 @@ import Header from "../../components/Header/Header";
 import Hero from "../../components/Hero/Hero";
 import Works from "../../components/Works/Works";
 
-export default function Home() {
+export default function Home({ data }) {
   return (
     <div className="h-screen w-full">
       <Header />
       <Hero />
-      <Works />
+      <Works data={data} />
     </div>
   );
 }
