ea564b36eaae48dbf504488b62722207605ad24d
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-07-03 12:01:59 -0300

    fix: adjust spacing

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,6 @@
 export default function Hero() {
   return (
-    <section className="mt-14 w-full h-[91vh] pt-8 pl-[4.7rem] flex">
+    <section className="mt-14 w-full h-[91vh] pt-8 pl-[11.2rem] flex">
       <div className="w-1/2">
         <p className="font-medium text-base text-content w-2/3 mt-10">
           Tenho 26 anos, desenvolvedor front-end com um ano de experiência
