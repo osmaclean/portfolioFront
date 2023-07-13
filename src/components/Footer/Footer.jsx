33655e06bc357c46c439090986c9c5239495f4a7
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-07-13 15:05:37 -0300

    added dark mode work and footer session

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,64 +3,118 @@ import GitHub from "/light/placa-do-github.svg";
 import LinkedIn from "/light/logotipo-do-linkedin.svg";
 import Instagram from "/light/instagram.svg";
 import Curriculo from "/light/curriculo.png";
+import CurriculoDark from "/dark/curriculoDark.png";
+import GitHubDark from "/dark/githubDark.png";
+import LinkedInDark from "/dark/linkedinDark.png";
+import InstagramDark from "/dark/instaDark.png";
+import { useTheme } from "../../hooks/useTheme";
 
 export default function Footer() {
+  const { theme } = useTheme();
   return (
     <section
       id="sectionFooter"
-      className="w-full pt-24 pl-[3rem] sm-1:pl-[1rem] sm-1:pr-[1rem] sm:pl-[2rem] sm:pr-[2rem] pb-11 flex items-center justify-center md:justify-start"
+      className="w-full bg-gradient-to-t from-gradient1 to-gradient2 dark:from-gradient-dark2 dark:to-gradient-dark1 pt-24 pl-[3rem] sm-1:pl-[1rem] sm-1:pr-[1rem] sm:pl-[2rem] sm:pr-[2rem] pb-11 flex items-center justify-center md:justify-start"
     >
       <div className="w-3/4">
-        <h1 className="text-content text-3xl font-bold md-3:text-2xl sm:text-xl l:text-4xl l-3:text-5xl">
+        <h1 className="text-content dark:text-header text-3xl font-bold md-3:text-2xl sm:text-xl l:text-4xl l-3:text-5xl">
           Entre em contato
         </h1>
-        <span className="mt-6 flex items-center justify-center hover:animate-pulse font-bold gap-2 p-4 uppercase shadow-button text-white bg-content rounded-md w-96 md-3:w-80 sm:w-64 sm:text-xs sm-2:w-56 sm-2:text-[.65rem] md-3:text-sm antialiased h-6 l:text-xl l:w-[28rem] l:h-10 l-3:text-[1.35rem] l-3:w-[30rem] l-3:h-12">
+        <span className="mt-6 flex items-center justify-center hover:animate-pulse font-bold gap-2 p-4 uppercase shadow-button text-white dark:text-content dark:bg-header bg-content rounded-md w-96 md-3:w-80 sm:w-64 sm:text-xs sm-2:w-56 sm-2:text-[.65rem] md-3:text-sm antialiased h-6 l:text-xl l:w-[28rem] l:h-10 l-3:text-[1.35rem] l-3:w-[30rem] l-3:h-12">
           contatolucasmaclean@gmail.com
         </span>
         <div className="flex items-center justify-start gap-6 mt-6 w-52 mb-9">
           <figure>
-            <img
-              src={GitHub}
-              alt="Logotipo GitHub"
-              aria-label="GitHub Botão"
-              className="cursor-pointer h-8 w-8 md-3:h-6 md-3:w-6 l:h-12 l:w-12 hover:opacity-60 transition-all"
-              onClick={() => {
-                open("https://github.com/osmaclean");
-              }}
-            />
+            {theme === "light" ? (
+              <img
+                src={GitHub}
+                alt="Logotipo GitHub"
+                aria-label="GitHub Botão"
+                className="cursor-pointer h-8 w-8 md-3:h-6 md-3:w-6 l:h-12 l:w-12 hover:opacity-60 transition-all"
+                onClick={() => {
+                  open("https://github.com/osmaclean");
+                }}
+              />
+            ) : (
+              <img
+                src={GitHubDark}
+                alt="Logotipo GitHub"
+                aria-label="GitHub Botão"
+                className="cursor-pointer h-8 w-8 md-3:h-6 md-3:w-6 l:h-12 l:w-12 hover:opacity-60 transition-all"
+                onClick={() => {
+                  open("https://github.com/osmaclean");
+                }}
+              />
+            )}
           </figure>
           <figure>
-            <img
-              src={LinkedIn}
-              alt="Logotipo LinkedIn"
-              aria-label="LinkedIn Botão"
-              className="cursor-pointer h-8 w-8 md-3:h-6 md-3:w-6 l:h-12 l:w-12 hover:opacity-60 transition-all"
-              onClick={() => {
-                open("https://www.linkedin.com/in/osmaclean/");
-              }}
-            />
+            {theme === "light" ? (
+              <img
+                src={LinkedIn}
+                alt="Logotipo LinkedIn"
+                aria-label="LinkedIn Botão"
+                className="cursor-pointer h-8 w-8 md-3:h-6 md-3:w-6 l:h-12 l:w-12 hover:opacity-60 transition-all"
+                onClick={() => {
+                  open("https://www.linkedin.com/in/osmaclean/");
+                }}
+              />
+            ) : (
+              <img
+                src={LinkedInDark}
+                alt="Logotipo LinkedIn"
+                aria-label="LinkedIn Botão"
+                className="cursor-pointer h-8 w-8 md-3:h-6 md-3:w-6 l:h-12 l:w-12 hover:opacity-60 transition-all"
+                onClick={() => {
+                  open("https://www.linkedin.com/in/osmaclean/");
+                }}
+              />
+            )}
           </figure>
           <figure>
-            <img
-              src={Instagram}
-              alt="Logotipo Instagram"
-              aria-label="Instagram Botão"
-              className="cursor-pointer h-8 w-8 md-3:h-6 md-3:w-6 l:h-12 l:w-12 hover:opacity-60 transition-all"
-              onClick={() => {
-                open("https://www.instagram.com/devstechbrasil/");
-              }}
-            />
+            {theme === "light" ? (
+              <img
+                src={Instagram}
+                alt="Logotipo Instagram"
+                aria-label="Instagram Botão"
+                className="cursor-pointer h-8 w-8 md-3:h-6 md-3:w-6 l:h-12 l:w-12 hover:opacity-60 transition-all"
+                onClick={() => {
+                  open("https://www.instagram.com/devstechbrasil/");
+                }}
+              />
+            ) : (
+              <img
+                src={InstagramDark}
+                alt="Logotipo Instagram"
+                aria-label="Instagram Botão"
+                className="cursor-pointer h-8 w-8 md-3:h-6 md-3:w-6 l:h-12 l:w-12 hover:opacity-60 transition-all"
+                onClick={() => {
+                  open("https://www.instagram.com/devstechbrasil/");
+                }}
+              />
+            )}
           </figure>
           <figure>
-            <img
-              src={Curriculo}
-              alt="Logotipo Curriculo"
-              aria-label="Curriculo Botão"
-              className="cursor-pointer h-8 w-8 md-3:h-6 md-3:w-6 l:h-[2.25rem] l:w-12 hover:opacity-60 transition-all"
-              onClick={() => {
-                open("https://rebrand.ly/Curriculum-LM");
-              }}
-            />
+            {theme === "light" ? (
+              <img
+                src={Curriculo}
+                alt="Logotipo Curriculo"
+                aria-label="Curriculo Botão"
+                className="cursor-pointer h-8 w-8 md-3:h-6 md-3:w-6 l:h-[2.25rem] l:w-12 hover:opacity-60 transition-all"
+                onClick={() => {
+                  open("https://rebrand.ly/Curriculum-LM");
+                }}
+              />
+            ) : (
+              <img
+                src={CurriculoDark}
+                alt="Logotipo Curriculo"
+                aria-label="Curriculo Botão"
+                className="cursor-pointer h-8 w-8 md-3:h-6 md-3:w-6 l:h-[2.25rem] l:w-12 hover:opacity-60 transition-all"
+                onClick={() => {
+                  open("https://rebrand.ly/Curriculum-LM");
+                }}
+              />
+            )}
           </figure>
         </div>
       </div>
--- src/components/WhatsFloater/WhatsFloater.jsx
+++ src/components/WhatsFloater/WhatsFloater.jsx
@@ -1,20 +1,38 @@
 import WhatsFigure from "/light/whats.png";
+import WhatsDark from "/dark/whatsDark.png";
+import { useTheme } from "../../hooks/useTheme";
 
 export default function WhatsFloater() {
+  const { theme } = useTheme();
+
   return (
     <section className=" fixed right-4 top-80 w-16 h-36 l:w-20 flex flex-col items-center justify-center animate-pulse gap-2">
-      <h5 className="font-bold text-sm l:text-lg l-1:text-xl">Whats</h5>
-      <div className="w-12 h-12 l:w-16 l:h-16 l-1:w-20 l-1:h-20 bg-content shadow-button rounded-full flex flex-col items-center justify-center">
+      <h5 className="font-bold text-sm l:text-lg l-1:text-xl dark:text-header">
+        Whats
+      </h5>
+      <div className="w-12 h-12 l:w-16 l:h-16 l-1:w-20 l-1:h-20 bg-content dark:bg-header shadow-button rounded-full flex flex-col items-center justify-center">
         <figure>
-          <img
-            className="cursor-pointer text-center w-7 h-7 l:w-8 l:h-8 l-1:h-10 l-1:w-10"
-            onClick={() => {
-              open("https://rebrand.ly/whats-portfolio");
-            }}
-            src={WhatsFigure}
-            alt="WhatsApp"
-            aria-label="WhatsApp Button"
-          />
+          {theme === "light" ? (
+            <img
+              className="cursor-pointer text-center w-7 h-7 l:w-8 l:h-8 l-1:h-10 l-1:w-10"
+              onClick={() => {
+                open("https://rebrand.ly/whats-portfolio");
+              }}
+              src={WhatsFigure}
+              alt="WhatsApp"
+              aria-label="WhatsApp Button"
+            />
+          ) : (
+            <img
+              className="cursor-pointer text-center w-7 h-7 l:w-8 l:h-8 l-1:h-10 l-1:w-10"
+              onClick={() => {
+                open("https://rebrand.ly/whats-portfolio");
+              }}
+              src={WhatsDark}
+              alt="WhatsApp"
+              aria-label="WhatsApp Button"
+            />
+          )}
         </figure>
       </div>
     </section>
