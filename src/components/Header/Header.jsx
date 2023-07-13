5b93b9b4d6b58429580839890a3774c53d15aa20
Lucas Maclean <contatolucasmaclean@gmail.com>
2023-07-13 15:09:00 -0300

    added dark shadow in header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="h-14 sm:h-16 l-3:p-8 w-full bg-header dark:bg-content shadow-md p-6 sm:p-2 flex items-center justify-center fixed top-0 z-50">
+    <header className="h-14 sm:h-16 l-3:p-8 w-full bg-header dark:bg-content shadow-md dark:shadow-header dark:shadow-sm p-6 sm:p-2 flex items-center justify-center fixed top-0 z-50">
       <nav className="flex items-center justify-between w-3/4 md:w-[90%] sm:flex-col sm:gap-2">
         <h1 className="text-2xl font-bold cursor-pointer text-content md2nd:text-xl sm:text-lg sm-2:text-base l:text-3xl l-3:text-4xl dark:text-header">
           LUCAS MACLEAN
